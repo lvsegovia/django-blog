214163bcb924c45d60bd0a126d0b1656e1d8cc4c
lvsegovia <leonardo_lvs@hotmail.com>
2021-11-16 14:57:36 -0800

    Assignment 8

--- blogging/views.py
+++ blogging/views.py
@@ -2,24 +2,23 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
 from django.shortcuts import render # ?
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
-'''
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")
+
+class BloggingListView(ListView):
+    '''
+    model = Post
+    '''
+    queryset = Post.objects.order_by('-published_date')
+    template_name = 'blogging/list.html'
+
+
+class BloggingDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+
+
 '''
 # rewrite our view, same as above but with a shortcut
 def list_view(request):
@@ -36,3 +35,4 @@ def detail_view(request, post_id):
         raise Http404
     context = {'post': post}
     return render(request, 'blogging/detail.html', context)
+'''
